945b87c0cd3b8bf256820b28871a0d96fc503fe2
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-04-27 12:19:50 +0200

    Configure TypeScript for jest

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -2,9 +2,15 @@ const path = require('path')
 
 module.exports = {
   rootDir: path.resolve(__dirname, '../../'),
+  globals: {
+    'ts-jest': {
+      tsConfigFile: 'tsconfig.json'
+    }
+  },
   moduleFileExtensions: [
     'js',
     'json',
+    'ts',
     'vue'
   ],
   moduleNameMapper: {
@@ -12,10 +18,14 @@ module.exports = {
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(ts)$': 'ts-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
   testPathIgnorePatterns: [
     '<rootDir>/test/e2e'
+  ],
+  testMatch: [
+    '**/test/**/*.(test|spec).(ts|js)'
   ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
