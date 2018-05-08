d9d796a674c79123161cf44d0c8159ae3241549b
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-08 17:35:53 +0200

    GitHub releases (#10)
    Used a 3rd party package to deploy on GitHub releases.
    The zip contains the content of the `dist` folder

--- release.sh
+++ release.sh
@@ -1,32 +1,26 @@
 #!/bin/sh
 
-if [ "x${GITHUB_API_TOKEN}" = "x" ]; then
-  echo "Please add the GITHUB_API_TOKEN env var"
-  exit 1
-fi
-
-if [ "x${CIRCLE_TAG}" = "x" || "x${CIRCLE_BRANCH}" = "x" ]; then
-  echo "Run this script in Circle CI (or setup CIRCLE_TAG and CIRCLE_BRANCH manually to test)"
-  exit 1
-fi
-
-ORG=socialement-competents
-REPO=hackafront
-RELEASES_URL=https://api.github.com/repos/${ORG}/${REPO}/releases
-
 # Add all files and folders, recursively, to a tag-named .zip 
-FILENAME="${REPO}-${CIRCLE_TAG}.zip"
-zip -r ${FILENAME} *
+FILENAME="$CIRCLE_PROJECT_REPONAME-$CIRCLE_TAG.zip"
+zip -r $FILENAME *
 
-# Make sure the release has already been created, otherwise create it
-PAYLOAD='{"tag_name":"'${CIRCLE_TAG}'","target_commitish":"'${CIRCLE_BRANCH}'","name":"'${CIRCLE_TAG}'","draft":false,"prerelease":false}'
-echo ${PAYLOAD}
-curl -i -X POST -H "Content-Type:application/json" -H "Authorization: token ${GITHUB_API_TOKEN}" ${RELEASES_URL} -d ${PAYLOAD}
+# Download the github-release executable
+GITHUB_RELEASE_GO=https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
+curl -sL $GITHUB_RELEASE_GO | tar xvjf -
+GR=bin/linux/amd64/github-release
+chmod +x $GR
 
-# Get the assets upload URL and truncate the last weird part
-UPLOAD_URL=$(curl -s "${RELEASES_URL}/tags/${CIRCLE_TAG}" | jq -r '.upload_url' | cut -d{ -f1)
-# Build the actual upload URL from it
-ASSET_URL="${UPLOAD_URL}/assets?name=${FILENAME}"
-# Send the file as binary in the request body
-curl -i -X POST -H "Content-Type:application/zip" -H "Authorization: token ${GITHUB_API_TOKEN}" ${ASSET_URL} --data-binary "@${FILENAME}"
-echo ""
+# Create a GitHub release
+$GR release \
+  --user $CIRCLE_PROJECT_USERNAME \
+  --repo $CIRCLE_PROJECT_REPONAME \
+  --tag $CIRCLE_TAG \
+  --name $CIRCLE_TAG
+
+# Upload the .zip to the release as an asset
+$GR upload \
+  --user $CIRCLE_PROJECT_USERNAME \
+  --repo $CIRCLE_PROJECT_REPONAME \
+  --tag $CIRCLE_TAG \
+  --name $FILENAME \
+  --file $FILENAME
