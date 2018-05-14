924b57127f8ae1a942f93564cbf177bc629aa06e
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-14 19:20:52 +0200

    Release cjs and umd zips

--- release.sh
+++ release.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Add all files and folders, recursively, to a tag-named .zip 
-FILENAME="$CIRCLE_PROJECT_REPONAME-$CIRCLE_TAG.zip"
-zip -r $FILENAME *
-
 # Download the github-release executable
 GITHUB_RELEASE_GO=https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
 curl -sL $GITHUB_RELEASE_GO | tar xvjf -
@@ -17,7 +13,24 @@ $GR release \
   --tag $CIRCLE_TAG \
   --name $CIRCLE_TAG
 
-# Upload the .zip to the release as an asset
+# Add all files and folders, recursively, to a tag-named .zip
+cd umd
+FILENAME="$CIRCLE_PROJECT_REPONAME-umd-$CIRCLE_TAG.zip"
+zip -r $FILENAME *
+
+# Upload the umd .zip to the release as an asset
+$GR upload \
+  --user $CIRCLE_PROJECT_USERNAME \
+  --repo $CIRCLE_PROJECT_REPONAME \
+  --tag $CIRCLE_TAG \
+  --name $FILENAME \
+  --file $FILENAME
+
+cd ../cjs
+FILENAME="$CIRCLE_PROJECT_REPONAME-cjs-$CIRCLE_TAG.zip"
+zip -r $FILENAME *
+
+# Upload the cjs .zip to the release as an asset
 $GR upload \
   --user $CIRCLE_PROJECT_USERNAME \
   --repo $CIRCLE_PROJECT_REPONAME \
