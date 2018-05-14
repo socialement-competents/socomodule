eb73a1c18356e4d86f92821e78277507dcd38405
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-14 19:44:40 +0200

    Improve the release generation script

--- release.sh
+++ release.sh
@@ -2,8 +2,7 @@
 
 # Download the github-release executable
 GITHUB_RELEASE_GO=https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
-curl -sL $GITHUB_RELEASE_GO | tar xvjf -
-GR=${PWD}/bin/linux/amd64/github-release
+GR=${PWD}/$(curl -sL $GITHUB_RELEASE_GO | tar xvjf -)
 chmod +x $GR
 
 # Create a GitHub release
@@ -15,25 +14,25 @@ $GR release \
 
 # Add all files and folders, recursively, to a tag-named .zip
 cd umd
-FILENAME="$CIRCLE_PROJECT_REPONAME-umd-$CIRCLE_TAG.zip"
-zip -r $FILENAME *
+UMD_FILENAME="$CIRCLE_PROJECT_REPONAME-umd-$CIRCLE_TAG.zip"
+zip -r $UMD_FILENAME *
 
 # Upload the umd .zip to the release as an asset
 $GR upload \
   --user $CIRCLE_PROJECT_USERNAME \
   --repo $CIRCLE_PROJECT_REPONAME \
   --tag $CIRCLE_TAG \
-  --name $FILENAME \
-  --file $FILENAME
+  --name $UMD_FILENAME \
+  --file $UMD_FILENAME
 
 cd ../cjs
-FILENAME="$CIRCLE_PROJECT_REPONAME-cjs-$CIRCLE_TAG.zip"
-zip -r $FILENAME *
+CJS_FILENAME="$CIRCLE_PROJECT_REPONAME-cjs-$CIRCLE_TAG.zip"
+zip -r $CJS_FILENAME *
 
 # Upload the cjs .zip to the release as an asset
 $GR upload \
   --user $CIRCLE_PROJECT_USERNAME \
   --repo $CIRCLE_PROJECT_REPONAME \
   --tag $CIRCLE_TAG \
-  --name $FILENAME \
-  --file $FILENAME
+  --name $CJS_FILENAME \
+  --file $CJS_FILENAME
