fb31197dc0257d6abc65112d765bdb516cd8bf75
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-15 17:16:25 +0200

    Auto publish to npm

---
+++ package/publish.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir tmp
+
+cat docs/Introduction.md >> tmp/Readme.md
+cat docs/UseCase1.md >> tmp/Readme.md
+cat docs/UseCase2.md >> tmp/Readme.md
+
+cp dist/cjs/* tmp/
+cp package/package.json tmp/package.json
+
+version=${CIRCLE_TAG#"v"}
+sed -i "s/1.0.0/${version}/g" tmp/package.json
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+npm publish tmp
