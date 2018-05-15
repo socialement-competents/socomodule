77077c2660f3fb182bdc6ba742d427ada4524fbd
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-15 18:24:08 +0200

    Comment the script

--- package/publish.sh
+++ package/publish.sh
@@ -2,15 +2,21 @@
 
 mkdir tmp
 
+# Append all the documentation into 1 file
 cat docs/Introduction.md >> tmp/Readme.md
 cat docs/UseCase1.md >> tmp/Readme.md
 cat docs/UseCase2.md >> tmp/Readme.md
 
+# Copy the required assets
 cp dist/cjs/* tmp/
 cp package/package.json tmp/package.json
 
+# Update the version defined in package.json to the tag version
 version=${CIRCLE_TAG#"v"}
 sed -i "s/1.0.0/${version}/g" tmp/package.json
 
+# Log in to npm with a token
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+
+# Publish a folder to npm
 npm publish tmp
